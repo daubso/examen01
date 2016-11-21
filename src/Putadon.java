77a9123ca7b46c27b6972e9c7653880bbb07e414
daubso <david.edreamer@gmail.com>
2016-11-21 15:53:43 +0100

    primer commit

--- src/Putadon.java
+++ src/Putadon.java
@@ -15,27 +15,42 @@ public class Putadon {
 	public static void main(String[] args) {
 		//Variables inicialización
 		String frase="";
-		int numPos=0;
+		int numPos = 0;
 		int numRepeticiones=0;
 		char letra=0;
-		
 		//Pedimos datos
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Introduce tu frase");
-		frase = sc.nextLine();
-		
-		System.out.println("Introduce la letra a buscar");
-		letra = sc.nextLine().charAt(0); //charAt(0) devuelve la primera letra
-		
+		introduceFrase("");
+		letraBuscar(letra);
 		//Buscamos las letras
-		for(int i=0;i<frase.length();i++){
-			if(letra==frase.charAt(i-1)){
-				numRepeticiones=numRepeticiones;
-				System.out.println("Letra "+letra+" encontrada en la posicion "+i);
-			}
-		}
-		System.out.println("Total repeticiones "+numRepeticiones);
+		buscaLetras(frase, letra, numRepeticiones, numPos);
+	}
 	
+	public static String introduceFrase(String frase){
+		Scanner sc=new Scanner(System.in);
+		imprimir("Introduce tu frase");
+		frase = sc.nextLine();
+		return frase;
+	}
+	public static char letraBuscar(char letra){
+		Scanner sc=new Scanner(System.in);
+		imprimir("Introduce la letra a buscar");
+		letra = sc.nextLine().charAt(0);
+		return letra;
+	}
+	public static void imprimir(String msg){
+		System.out.println(msg);
+		
+	}
+	public static void buscaLetras(String frase,char letra,int numRepeticiones, int numPos){
+		for(int i=0;i<frase.length();i++){
+			
+				letra=frase.charAt(i+1);
+				numRepeticiones=numRepeticiones+1;
+				numPos=numPos+letra;
+				imprimir("Letra "+letra+" encontrada en la posicion "+numPos);
+			
+		}
+		imprimir("Letra "+letra+" encontrada en la posicion "+numPos);
+		imprimir("Total repeticiones "+numRepeticiones);
 	}
 }
